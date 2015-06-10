fc4917234df215efcc1569f3a34e693c22815d30
Billy Tobon <btobon@renttherunway.com>
2015-06-10 08:00:58 -0700

    Separating rent button into a dynamic framework

---
+++ RentTheGown/RentKit/RentKit.h
@@ -0,0 +1,20 @@
+//
+//  RentKit.h
+//  RentKit
+//
+//  Created by Billy Tobon on 6/10/15.
+//  Copyright (c) 2015 Rent The Runway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RentKit.
+FOUNDATION_EXPORT double RentKitVersionNumber;
+
+//! Project version string for RentKit.
+FOUNDATION_EXPORT const unsigned char RentKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RentKit/PublicHeader.h>
+
+#import <RentKit/RENTButton.h>
+
